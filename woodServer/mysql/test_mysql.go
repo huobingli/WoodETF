166e42e15cd91a64a2b08d40d3250b97ee3587a8
huobingli <huobingli@outlook.com>
2021-09-03 17:54:05 +0800

    update

--- woodServer/mysql/test_mysql.go
+++ woodServer/mysql/test_mysql.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
@@ -9,9 +10,34 @@ import (
 
 var Db *sqlx.DB
 
+type ARK_ETF struct {
+	Ark_Date         string `db:"ark_date"`
+	Ark_Stock_Name   string `db:"ark_stock_name"`
+	Ark_Shares       string `db:"ark_shares"`
+	Ark_Market_Value string `db:"ark_market_value"`
+	Ark_Weight       string `db:"ark_weight"`
+}
+
+func get_data() {
+	rows, err := Db.Query("SELECT ark_date,ark_stock_name,ark_shares,ark_market_value,ark_weight FROM ARKK_ETF WHERE ark_stock_name=?", "TSLA")
+	if err != nil {
+		fmt.Printf(`%T`, rows)
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	// fmt.Print(rows)
+	for rows.Next() {
+		var ark_stock ARK_ETF
+		if err := rows.Scan(&ark_stock.Ark_Date, &ark_stock.Ark_Stock_Name, &ark_stock.Ark_Shares, &ark_stock.Ark_Market_Value, &ark_stock.Ark_Weight); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Print(ark_stock)
+	}
+}
+
 func init() {
 
-	database, err := sqlx.Open("mysql", "root:@tcp(127.0.0.1:3306)/test")
+	database, err := sqlx.Open("mysql", "root:111111@tcp(47.114.171.118:32333)/ark_test")
 	if err != nil {
 		fmt.Println("open mysql failed,", err)
 		return
@@ -30,21 +56,15 @@ type Person struct {
 }
 
 func main() {
-	var person []Person
+	// var person []Person
 
-	err := Db.Select(&person, "select a, b, c, d, e from test where id=?", 1)
-	if err != nil {
-		fmt.Println("exec failed, ", err)
-		return
-	}
-
-	fmt.Println("select succ:", person)
-
-	// stmt, err := db.Prepare("alter table dev convert to character set utf8 collate utf8_general_ci")  //要修改一下编码
-	// if stmt != nil {
-	//     stmt.Exec()
-	//     stmt.Close()
+	// err := Db.Select(&person, "select a, b, c, d, e from test where id=?", 1)
+	// if err != nil {
+	// 	fmt.Println("exec failed, ", err)
+	// 	return
 	// }
 
-	// _ = err
+	// fmt.Println("select succ:", person)
+
+	get_data()
 }
